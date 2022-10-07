1a6e997e6d4e4972a7e4afdd0c83474e8e98f36b
Eduardo Luz <eduluz1976@gmail.com>
2022-10-06 23:40:56 -0600

    Update PHPTestBase.php

--- tests/unit/kdb/processor/PHPTestBase.php
+++ tests/unit/kdb/processor/PHPTestBase.php
@@ -5,25 +5,28 @@ namespace tests\unit\SysKDB\kdb\processor;
 use PHPUnit\Framework\TestCase;
 use SysKDB\kdb\processor\PHP;
 
-
-class PHPTestBase extends TestCase {
-
-
-    public $processor; 
+class PHPTestBase extends TestCase
+{
+    public $processor;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->processor = new PHP();
-        
     }
-    
 
-    protected function parseAndProcess($statement) {
+
+    protected function parseAndProcess($statement)
+    {
         $contents = '<?php ' . $statement;
-        $tokens = $this->processor->parse($contents);
-        $this->processor->processTokens($tokens);
+        $this->getProcessor()->parseAndProcess($contents);
     }
 
-
+    /**
+     * Get the value of processor
+     */
+    public function getProcessor()
+    {
+        return $this->processor;
+    }
 }
