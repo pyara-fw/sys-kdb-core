4f47a0ad191e9a9dad3eea0ac4f5d934f447b6da
Eduardo Luz <eduluz1976@gmail.com>
2022-10-07 00:29:45 -0600

    Added the remaining classes from Core package

---
+++ src/kdm/lib/omg/mof/DataType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SysKDB\kdm\lib\omg\mof;
+
+/**
+ * DataType is the superclass of the classes
+ * that represent MOF data types and data type constructors
+ */
+abstract class DataType
+{
+}
