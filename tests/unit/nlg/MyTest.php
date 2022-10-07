19f2f973f0a50f60bc693a2cb5a113cc48e9769f
Eduardo Luz <eduluz1976@gmail.com>
2022-10-06 23:42:56 -0600

    Update MyTest.php

--- tests/unit/nlg/MyTest.php
+++ tests/unit/nlg/MyTest.php
@@ -3,20 +3,20 @@
 use PHPUnit\Framework\TestCase;
 use Leuffen\TextTemplate\TextTemplate;
 
-class MyTest extends TestCase {
-
-
-    protected function getTT($tplStr) {
+class MyTest extends TestCase
+{
+    protected function getTT($tplStr)
+    {
         $tt = new TextTemplate($tplStr);
 
 
-        $tt->addSection("paragraph", function ($content, $params, $command, $context, $cmdParam, $self) {            
-            $x = str_replace(["\n","\r","\t","  "]," ",trim($content));
+        $tt->addSection("paragraph", function ($content, $params, $command, $context, $cmdParam, $self) {
+            $x = str_replace(["\n","\r","\t","  "], " ", trim($content));
             return $x;
         });
 
 
-        $tt->addFilter('singular_plural', function($input, $singular=null, $plural=null, $inexistent=null){
+        $tt->addFilter('singular_plural', function ($input, $singular=null, $plural=null, $inexistent=null) {
             $n = 0;
             if (is_array($input)) {
                 $n = count($input);
@@ -27,11 +27,11 @@ class MyTest extends TestCase {
                 return $plural;
             } elseif ($n == 0 && $inexistent) {
                 return $inexistent;
-            } 
+            }
             return $singular;
         });
 
-        $tt->addFilter('join_list_words', function($input){
+        $tt->addFilter('join_list_words', function ($input) {
             if (!is_array($input)) {
                 return $input;
             }
@@ -39,16 +39,15 @@ class MyTest extends TestCase {
                 return reset($input);
             }
             $last = array_pop($input);
-            $output = join($input, ',');
+            $output = join(',', $input);
             $output .= ", and $last";
             return $output;
         });
 
-        $tt->addFilter('ama', function($input){
-
+        $tt->addFilter('ama', function ($input) {
             $article = 'an';
 
-            $chr = strtolower(substr($input,0,1));
+            $chr = strtolower(substr($input, 0, 1));
             if (strpos('aeiou', $chr) === false) {
                 $article = 'a';
             }
@@ -59,7 +58,8 @@ class MyTest extends TestCase {
     }
 
 
-    public function provider_sentence_class() {
+    public function provider_sentence_class()
+    {
         return [
             [
                 ['className' => 'Engine'],
@@ -80,33 +80,35 @@ class MyTest extends TestCase {
     /**
      * singular/plural based in a var, numerical token
      *      year/years
-     * 
+     *
      * singular/plural based in an array var
      *      is/are
-     * 
+     *
      * based on next word (vowel/consonant)
-     *      a/am 
-     * 
+     *      a/am
+     *
      * lists joining
      *      x / x,y, and z
-     * 
+     *
      * @dataProvider provider_sentence_class
      * @return void
      */
-    function test_sentence_class($data, $expected) {
+    public function test_sentence_class($data, $expected)
+    {
         $tplStr = '{paragraph name="class"}{trim}
 {= className}
 is a class
 {if extendsFrom != null }
 that extends from class {=extendsFrom}
 {/if}.
-{/trim}{/paragraph}';        
+{/trim}{/paragraph}';
 
-        $this->assertEquals($expected, $this->getTT($tplStr)->apply ($data));
+        $this->assertEquals($expected, $this->getTT($tplStr)->apply($data));
     }
 
 
-    function provider_sentence_interfaces() {
+    public function provider_sentence_interfaces()
+    {
         return [
             [
                 [
@@ -130,11 +132,12 @@ that extends from class {=extendsFrom}
     }
 
     /**
-     * 
+     *
      * @dataProvider provider_sentence_interfaces
      * @return void
      */
-    function test_sentence_interfaces($data, $expected) {
+    public function test_sentence_interfaces($data, $expected)
+    {
         $tplStr = '{paragraph name="interfaces"}{trim}{if interfaces }
 It implements the {=interfaces |singular_plural:interface:interfaces}
 {=interfaces |join_list_words}.
@@ -143,13 +146,14 @@ It implements the {=interfaces |singular_plural:interface:interfaces}
         $tt = $this->getTT($tplStr);
 
 
-        $this->assertEquals($expected, $tt->apply ($data));        
-        // $this->assertEquals($expected, $this->getTT($tplStr)->apply ($data));        
+        $this->assertEquals($expected, $tt->apply($data));
+        // $this->assertEquals($expected, $this->getTT($tplStr)->apply ($data));
     }
 
 
 
-    function provider_additional() {
+    public function provider_additional()
+    {
         return [
             [
                 [
@@ -167,16 +171,15 @@ It implements the {=interfaces |singular_plural:interface:interfaces}
     }
 
     /**
-     * 
+     *
      * @dataProvider provider_additional
      * @return void
      */
-    function test_additional($data, $expected) {
+    public function test_additional($data, $expected)
+    {
         $tplStr = '{= word|ama}';
 
         $tt = $this->getTT($tplStr);
-        $this->assertEquals($expected, $tt->apply ($data));        
+        $this->assertEquals($expected, $tt->apply($data));
     }
-
-
 }
